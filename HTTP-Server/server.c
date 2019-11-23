0c3d69de1f8cd39a3b7eb7a09a96ab9afe54afbe
teng <tteng1992@gmail.com>
2019-11-23 11:49:28 -0500

    the server processes the request, sending back its response, providing a status code and appropriate data

--- HTTP-Server/server.c
+++ HTTP-Server/server.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/epoll.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -113,9 +114,94 @@ void do_accept(int lfd, int epfd) {
     }
 }
 
+void disconnect(int cfd, int epfd) {
+    int ret = epoll_ctl(epfd, EPOLL_CTL_DEL, cfd, NULL);
+    if (ret != 0) {
+        perror("epoll_ctl error");
+        exit(1);
+    }
+    close(cfd);
+}
+
+/*  send server local files to the browser  */
+void send_file(int cfd, const char *file) {
+    int n = 0;
+    char buf[1024];
+    
+    int fd = open(file, O_RDONLY);
+    if (fd == -1) {
+        //  send 404 error page to browser
+        perror("open error");
+        exit(1);        
+    }
+    
+    while ((n = read(fd, buf, sizeof(buf))) > 0)
+        send(cfd, buf, n, 0);
+        
+    close(fd);
+}
+
+/**
+ * send the HTTP response
+ * @param  {cfd}  connection file descriptor  
+ * @param  {error_no}  error number  
+ * @param  {description}  error description  
+ * @param  {type}  file type  
+ * @param  {len}  file length  
+ * @return {void}
+*/
+void send_response(int cfd, int error_no, char* description, char* type, int len) {
+    char buf[1024] = {0};
+    sprintf(buf, "HTTP/1.1 %d %s\r\n", error_no, description);
+    sprintf(buf + strlen(buf), "%s\r\n", type);
+    sprintf(buf + strlen(buf), "Content-Length:%d\r\n", len);
+    send(cfd, buf, strlen(buf), 0);
+    send(cfd, "\r\n", 2, 0);
+}
+
+/*  the server processes the request, sending back its response, providing a status code and appropriate data  */
+void http_request(const char *file) {
+    struct stat sbuf;
+    
+    int ret = stat(file, &sbuf);                                //  get file status and check if the file exists
+    if (ret != 0) {
+        //  send 404 error page to browser
+        perror("stat error");
+        exit(1);        
+    }
+    
+    if (S_ISREG(sbuf.s_mode)) {                                 //  the file is a regular file
+        send_response(cfd, 200, "OK", "Content-Type: text/plain; charset=iso-8859-1", sbuf.st_size);    //  send the HTTP response                                       //  send the HTTP response
+        send_file(cfd, file);                                   //  send server local files to the browser
+    }
+}
+
 /*  read data  */
 void do_read(int cfd, int epfd) {
-    
+    char line[1024] = {0};
+    int line = get_line(cfd, line, sizeof(line));
+    if (line == 0) {
+        printf("the client %d closed...\n", cfd);
+        disconnect(cfd, epfd);
+    } else {
+        char method[16], path[256], protocol[16];
+        sscanf(line, "%[^ ] %[^ ] %[^ ]", method, path, protocol);
+        printf("method = %s, path = %s, protocol = %s\n", method, path, protocol);
+        
+        while (1) {
+            char buf[1024] = {0};
+            line = get_line(cfd, buf, sizeof(buf));
+            if (len == '\n')
+                break;
+            else if (len == -1)
+                break;
+        }
+        
+        if (strncasecmp(method, "GET", 3) == 0) {
+            char *file = path + 1;                              //  get the file name the client wants to access
+            http_request(cfd, file);
+        }
+    }
 }
 
 /*  use epoll to wait for an I/O event  */
